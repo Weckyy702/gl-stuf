1e142359f3ea2931f22ed2af0e12d893bc6ff90e
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2024-10-01 21:40:58 +0200

    Clean slate

--- 01-intro.cpp
+++ 01-intro.cpp
@@ -1,23 +1,3 @@
-#include "Context.hpp"
-#include "Window.hpp"
-#include <cmath>
-
-#include "glad/gl.h"
 #include <GLFW/glfw3.h>
 
-int main() {
-  auto ctx = opengl::Context::make();
-
-  auto window = opengl::WindowBuilder(ctx).build();
-  window.make_current();
-
-  float b = 0.;
-  while (!window.should_close()) {
-    auto const ctx = window.begin_draw();
-
-    b = static_cast<float>(sin(glfwGetTime()) * .5 + .5);
-
-    glClearColor(.5, 0., b, 1.);
-    glClear(GL_COLOR_BUFFER_BIT);
-  }
-}
+int main() {}
